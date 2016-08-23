143cd92616b9370ad860599debe29d22d741bbe8
Pimentel Rogerio-B19259 <B19259@freescale.com>
2016-08-23 13:53:25 -0300

    Update on cpmem tool
    Signed-off-by: Pimentel Rogerio-B19259 <B19259@freescale.com>

--- tools/dist/js/cpmem.js
+++ tools/dist/js/cpmem.js
@@ -3,59 +3,66 @@ var w0b0hex = "0", w0b1hex = "0", w0b2hex = "0", w0b3hex = "0";
 var w0b0hex2bin = "0", w0b1hex2bin = "0", w0b2hex2bin = "0", w0b3hex2bin = "0";
 var w1b0hex = "0", w1b1hex = "0", w1b2hex = "0", w1b3hex = "0";
 var w1b0hex2bin = "0", w1b1hex2bin = "0", w1b2hex2bin = "0", w1b3hex2bin = "0";
-var w0b0bin = "0", w0b1bin = "0";
-var w0b0bin2hex = "0", w0b1bin2hex = "0";
+var w0bin = "0", w1bin = "0";
+var w0bin2hex = "0", w1bin2hex = "0";
 
 
-// ni = ["name", "start", "size", "HEX", "BIN", "DEC"]
+// ni = ["name", "word", "start", "size", "HEX", "BIN", "DEC"]
 
 var ni = [
-    ["xv", 0, 10, "0", "0", "0"],["yv", 10, 9, "0", "0", "0"],
-    ["xb", 19, 13, "0", "0", "0"],["yb", 32, 12, "0", "0", "0"],
-    ["nsbb", 44, 1, "0", "0", "0"],["cf", 45, 1, "0", "0", "0"],
-    ["ubo", 46, 22, "0", "0", "0"],["vbo", 68, 22, "0", "0", "0"],
-    ["iox", 90, 4, "0", "0", "0"],["rdrw", 94, 1, "0", "0", "0"],
-    ["so", 113, 1, "0", "0", "0"],["bndm", 114, 3, "0", "0", "0"],
-    ["bm", 117, 2, "0", "0", "0"],["rot", 119, 1, "0", "0", "0"],
-    ["hf", 120, 1, "0", "0", "0"],["vf", 121, 1, "0", "0", "0"],
-    ["the", 122, 1, "0", "0", "0"],["cap", 123, 1, "0", "0", "0"],
-    ["cae", 124, 1, "0", "0", "0"],["fw", 125, 13, "0", "0", "0"],
-    ["fh", 138, 12, "0", "0", "0"],["eoli", 150, 1, "0", "0", "0"],
+    ["xv", 0, 0, 10, "0", "0", "0"],["yv", 0, 10, 9, "0", "0", "0"],
+    ["xb", 0, 19, 13, "0", "0", "0"],["yb", 0, 32, 12, "0", "0", "0"],
+    ["nsbb", 0, 44, 1, "0", "0", "0"],["cf", 0, 45, 1, "0", "0", "0"],
+    ["ubo", 0, 46, 22, "0", "0", "0"],["vbo", 0, 68, 22, "0", "0", "0"],
+    ["iox", 0, 90, 4, "0", "0", "0"],["rdrw", 0, 94, 1, "0", "0", "0"],
+    ["so", 0, 113, 1, "0", "0", "0"],["bndm", 0, 114, 3, "0", "0", "0"],
+    ["bm", 0, 117, 2, "0", "0", "0"],["rot", 0, 119, 1, "0", "0", "0"],
+    ["hf", 0, 120, 1, "0", "0", "0"],["vf", 0, 121, 1, "0", "0", "0"],
+    ["the", 0, 122, 1, "0", "0", "0"],["cap", 0, 123, 1, "0", "0", "0"],
+    ["cae", 0, 124, 1, "0", "0", "0"],["fw", 0, 125, 13, "0", "0", "0"],
+    ["fh", 0, 138, 12, "0", "0", "0"],["eoli", 0, 150, 1, "0", "0", "0"],
     
-    ["eba0", 0, 29, "0", "0", "0"],["eba1", 29, 29, "0", "0", "0"],
-    ["ilo", 58, 20, "0", "0", "0"],["npb", 78, 8, "0", "0", "0"],
-    ["pfs", 85, 4, "0", "0", "0"],["alu", 89, 1, "0", "0", "0"],
-    ["albm", 90, 2, "0", "0", "0"],["id", 93, 2, "0", "0", "0"],
-    ["th", 95, 7, "0", "0", "0"],["sly", 102, 14, "0", "0", "0"],
-    ["wid3", 125, 3, "0", "0", "0"],["sluv", 128, 14, "0", "0", "0"],
-    ["cre", 149, 1, "0", "0", "0"]  
+    ["eba0", 1, 0, 29, "0", "0", "0"],["eba1", 1, 29, 29, "0", "0", "0"],
+    ["ilo", 1, 58, 20, "0", "0", "0"],["npb", 1, 78, 8, "0", "0", "0"],
+    ["pfs", 1, 85, 4, "0", "0", "0"],["alu", 1, 89, 1, "0", "0", "0"],
+    ["albm", 1, 90, 2, "0", "0", "0"],["id", 1, 93, 2, "0", "0", "0"],
+    ["th", 1, 95, 7, "0", "0", "0"],["sly", 1, 102, 14, "0", "0", "0"],
+    ["wid3", 1, 125, 3, "0", "0", "0"],["sluv", 1, 128, 14, "0", "0", "0"],
+    ["cre", 1, 149, 1, "0", "0", "0"]  
 ];
 
 var intl = [
-    ["xv", 0, 10, "0", "0", "0"],["yv", 10, 9, "0", "0", "0"],
-    ["xb", 19, 13, "0", "0", "0"],["yb", 32, 12, "0", "0", "0"],
-    ["nsbb", 44, 1, "0", "0", "0"],["cf", 45, 1, "0", "0", "0"],
-    ["ubo", 46, 22, "0", "0", "0"],["vbo", 68, 22, "0", "0", "0"],
-    ["iox", 90, 4, "0", "0", "0"],["rdrw", 94, 1, "0", "0", "0"],
-    ["so", 113, 1, "0", "0", "0"],["bndm", 114, 3, "0", "0", "0"],
-    ["bm", 117, 2, "0", "0", "0"],["rot", 119, 1, "0", "0", "0"],
-    ["hf", 120, 1, "0", "0", "0"],["vf", 121, 1, "0", "0", "0"],
-    ["the", 122, 1, "0", "0", "0"],["cap", 123, 1, "0", "0", "0"],
-    ["cae", 124, 1, "0", "0", "0"],["fw", 125, 13, "0", "0", "0"],
-    ["fh", 138, 12, "0", "0", "0"],["eoli", 150, 1, "0", "0", "0"],
+    ["xv", 0, 0, 10, "0", "0", "0"],["yv", 0, 10, 9, "0", "0", "0"],
+    ["xb", 0, 19, 13, "0", "0", "0"],["yb", 0, 32, 12, "0", "0", "0"],
+    ["nsbb", 0, 44, 1, "0", "0", "0"],["cf", 0, 45, 1, "0", "0", "0"],
+    ["ubo", 0, 46, 22, "0", "0", "0"],["vbo", 0, 68, 22, "0", "0", "0"],
+    ["iox", 0, 90, 4, "0", "0", "0"],["rdrw", 0, 94, 1, "0", "0", "0"],
+    ["so", 0, 113, 1, "0", "0", "0"],["bndm", 0, 114, 3, "0", "0", "0"],
+    ["bm", 0, 117, 2, "0", "0", "0"],["rot", 0, 119, 1, "0", "0", "0"],
+    ["hf", 0, 120, 1, "0", "0", "0"],["vf", 0, 121, 1, "0", "0", "0"],
+    ["the", 0, 122, 1, "0", "0", "0"],["cap", 0, 123, 1, "0", "0", "0"],
+    ["cae", 0, 124, 1, "0", "0", "0"],["fw", 0, 125, 13, "0", "0", "0"],
+    ["fh", 0, 138, 12, "0", "0", "0"],["eoli", 0, 150, 1, "0", "0", "0"],
     
-    ["eba0", 0, 29, "0", "0", "0"],["eba1", 29, 29, "0", "0", "0"],
-    ["ilo", 58, 20, "0", "0", "0"],["npb", 78, 8, "0", "0", "0"],
-    ["pfs", 85, 4, "0", "0", "0"],["alu", 89, 1, "0", "0", "0"],
-    ["albm", 90, 2, "0", "0", "0"],["id", 93, 2, "0", "0", "0"],
-    ["th", 95, 7, "0", "0", "0"],["sly", 102, 14, "0", "0", "0"],
-    ["wid3", 125, 3, "0", "0", "0"],["sluv", 128, 14, "0", "0", "0"],
-    ["cre", 149, 1, "0", "0", "0"]  
+    ["eba0", 1, 0, 29, "0", "0", "0"],["eba1", 1, 29, 29, "0", "0", "0"],
+    ["ilo", 1, 58, 20, "0", "0", "0"],["npb", 1, 78, 8, "0", "0", "0"],
+    ["pfs", 1, 85, 4, "0", "0", "0"],["alu", 1, 89, 1, "0", "0", "0"],
+    ["albm", 1, 90, 2, "0", "0", "0"],["id", 1, 93, 2, "0", "0", "0"],
+    ["th", 1, 95, 7, "0", "0", "0"],["sly", 1, 102, 14, "0", "0", "0"],
+    ["wid3", 1, 125, 3, "0", "0", "0"],["sluv", 1, 128, 14, "0", "0", "0"],
+    ["cre", 1, 149, 1, "0", "0", "0"]  
 ];
+
+var reghex8char = new RegExp(/^[A-Fa-f0-9]{8}$/i);
+
 
 //Hex fields blur
 $("#w0b0hex").blur(function(){
     w0b0hex = $("#w0b0hex").val();
+
+    if (!reghex8char.test(w0b0hex))
+        alert('Please, fill with a 8-digit hex value. E.g. 1234abcd');
+    
     ret = hexToBinary(w0b0hex);
     w0b0hex2bin = ret['result'];
     update_word0_bin();
@@ -64,36 +71,96 @@ $("#w0b0hex").blur(function(){
 
 $("#w0b1hex").blur(function(){
     w0b1hex = $("#w0b1hex").val();
+    
+    if (!reghex8char.test(w0b1hex))
+        alert('Please, fill with a 8-digit hex value. E.g. 1234abcd');    
+    
     ret = hexToBinary(w0b1hex);
     w0b1hex2bin = ret['result'];
     update_word0_bin();
-    update_ni_hex();
+    update_ni();
 });
 
 $("#w0b2hex").blur(function(){
     w0b2hex = $("#w0b2hex").val();
+    
+    if (!reghex8char.test(w0b2hex))
+        alert('Please, fill with a 8-digit hex value. E.g. 1234abcd');    
+    
     ret = hexToBinary(w0b2hex);
     w0b2hex2bin = ret['result'];
     update_word0_bin();
-    update_ni_hex();
+    update_ni();
 });
 
 $("#w0b3hex").blur(function(){
     w0b3hex = $("#w0b3hex").val();
+    
+    if (!reghex8char.test(w0b3hex))
+        alert('Please, fill with a 8-digit hex value. E.g. 1234abcd');    
+    
     ret = hexToBinary(w0b3hex);
     w0b3hex2bin = ret['result'];
     update_word0_bin();
-    update_ni_hex();
+    update_ni();
+});
+
+$("#w1b0hex").blur(function(){
+    w1b0hex = $("#w1b0hex").val();
+
+    if (!reghex8char.test(w1b0hex))
+        alert('Please, fill with a 8-digit hex value. E.g. 1234abcd');
+    
+    ret = hexToBinary(w1b0hex);
+    w1b0hex2bin = ret['result'];
+    update_word1_bin();
+    update_ni();
+});
+
+$("#w1b1hex").blur(function(){
+    w1b1hex = $("#w1b1hex").val();
+    
+    if (!reghex8char.test(w1b1hex))
+        alert('Please, fill with a 8-digit hex value. E.g. 1234abcd');    
+    
+    ret = hexToBinary(w1b1hex);
+    w1b1hex2bin = ret['result'];
+    update_word1_bin();
+    update_ni();
+});
+
+$("#w1b2hex").blur(function(){
+    w1b2hex = $("#w1b2hex").val();
+    
+    if (!reghex8char.test(w1b2hex))
+        alert('Please, fill with a 8-digit hex value. E.g. 1234abcd');    
+    
+    ret = hexToBinary(w1b2hex);
+    w1b2hex2bin = ret['result'];
+    update_word1_bin();
+    update_ni();
+});
+
+$("#w1b3hex").blur(function(){
+    w1b3hex = $("#w1b3hex").val();
+    
+    if (!reghex8char.test(w1b3hex))
+        alert('Please, fill with a 8-digit hex value. E.g. 1234abcd');    
+    
+    ret = hexToBinary(w1b3hex);
+    w1b3hex2bin = ret['result'];
+    update_word1_bin();
+    update_ni();
 });
 
 //Bin fields blur
-$("#w0b0bin").blur(function(){
+$("#w0bin").blur(function(){
 
-    w0b0bin = $("#w0b0bin").val();
-    ret = binaryToHex(w0b0bin);
-    w0b0bin2hex = ret['result'];
+    w0bin = $("#w0bin").val();
+    ret = binaryToHex(w0bin);
+    w0bin2hex = ret['result'];
     update_word0_hex();
-    update_ni_hex();
+    update_ni();
  
 });
 
@@ -102,16 +169,25 @@ function update_word0_bin(){
     value = w0b0hex2bin.concat(w0b1hex2bin);
     value = value.concat(w0b2hex2bin);
     value = value.concat(w0b3hex2bin); 
-    w0b0bin = value;
-    $("#w0b0bin").val(value);
+    w0bin = value;
+    $("#w0bin").val(value);
+}
+
+function update_word1_bin(){
+
+    value = w1b0hex2bin.concat(w1b1hex2bin);
+    value = value.concat(w1b2hex2bin);
+    value = value.concat(w1b3hex2bin); 
+    w1bin = value;
+    $("#w1bin").val(value);
 }
 
 function update_word0_hex(){
     
-    w0b0hex = w0b0bin2hex.substr(0,4);
-    w0b1hex = w0b0bin2hex.substr(4,4);
-    w0b2hex = w0b0bin2hex.substr(8,4);
-    w0b3hex = w0b0bin2hex.substr(12,4);
+    w0b0hex = w0bin2hex.substr(0,4);
+    w0b1hex = w0bin2hex.substr(4,4);
+    w0b2hex = w0bin2hex.substr(8,4);
+    w0b3hex = w0bin2hex.substr(12,4);
     
     $("#w0b0hex").val(w0b0hex);
     $("#w0b1hex").val(w0b1hex);
@@ -123,31 +199,32 @@ function update_ni(){
     
     for (i = 0; i < ni.length; i++) { 
         
+        if (ni[i][1])
+            binaryword = w1bin.substr(ni[i][2],ni[i][3]);
+        else
+            binaryword = w0bin.substr(ni[i][2],ni[i][3]);
         //update hex
-        ret = w0b0bin.substr(ni[i][1],ni[i][2]);
-        ret = binaryToHex(ret);
-        ni[i][3] = ret["result"];
+        ret = binaryToHex(binaryword);
+        ni[i][4] = ret["result"];
         var fieldname = "#ni_";
         fieldname = fieldname.concat(ni[i][0]);
         fieldname = fieldname.concat("_hex");
-        $(fieldname).val(ni[i][3]);
+        $(fieldname).val(ni[i][4]);
         
         //update bin
-        ret = w0b0bin.substr(ni[i][1],ni[i][2]);
-        ni[i][4] = ret;
+        ni[i][5] = binaryword;
         var fieldname = "#ni_";
         fieldname = fieldname.concat(ni[i][0]);
         fieldname = fieldname.concat("_bin");
-        $(fieldname).val(ni[i][4]);
+        $(fieldname).val(ni[i][5]);
 
         //update dec
-        ret = w0b0bin.substr(ni[i][1],ni[i][2]);
-        ret = parseInt(ret, 2);
-        ni[i][5] = ret;
+        ret = parseInt(binaryword, 2);
+        ni[i][6] = ret;
         var fieldname = "#ni_";
         fieldname = fieldname.concat(ni[i][0]);
         fieldname = fieldname.concat("_dec");
-        $(fieldname).val(ni[i][5]);        
+        $(fieldname).val(ni[i][6]);        
         
     }
     
